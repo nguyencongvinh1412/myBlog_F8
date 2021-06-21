0e44d5971c030834c1136deb0f88dd6e1e448a34
nguyencongvinh1412 <nguyencongvinh@gmail.com>
2021-06-21 15:15:20 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send("Hello Vinh"))
+
+app.listen(port, (req, res) => {
+    console.log("Listening on port: " + port);
+})
